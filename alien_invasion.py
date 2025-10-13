554f8a7a8c43dc2fab6aa2af4b8b1eb40a04c5dc
batar-btr <btrn86@gmail.com>
2025-10-13 20:08:51 +0300

    feat: create game Settings class

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """The main class to manage game behavior and assets."""
@@ -9,9 +11,14 @@ class AlienInvasion:
     def __init__(self):
         """Init game and create game resources"""
         pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))  # surface
 
-        self.screen = pygame.display.set_mode((1200, 800))  # surface
         pygame.display.set_caption("Alien Invasion")
+
+        self.bg_color = self.settings.bg_color
 
     def run_game(self):
         """Run main game loop"""
@@ -20,9 +27,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            self.screen.fill(self.bg_color)
             # Show last drawed screen
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == "__main__":
