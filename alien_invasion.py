29ec3c97c1a69da1e159bdea4439b0fa1575104e
batar-btr <btrn86@gmail.com>
2025-10-15 00:12:15 +0300

    refactor: create event helper methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,15 +37,9 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
 
     def _update_screen(self):
         """Update image and new screen"""
@@ -53,6 +47,20 @@ class AlienInvasion:
         self.ship.blitme()
         # Show last drawed screen
         pygame.display.flip()
+
+    def _check_keydown_events(self, event):
+        """Reacts to keydown"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Reacts to keyup"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
 
 if __name__ == "__main__":
