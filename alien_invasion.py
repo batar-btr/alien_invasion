1d4b1b82b68d9266a42bd1f27e56bc2ec63a002b
batar-btr <btrn86@gmail.com>
2025-10-15 17:22:36 +0300

    feat: remove bullets on top

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,6 +36,12 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
+
             self._update_screen()
             self.clock.tick(60)
 
