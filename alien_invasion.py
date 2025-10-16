220ea11831f08628d0c0118ab58760fb32569c01
batar-btr <btrn86@gmail.com>
2025-10-16 22:02:44 +0300

    refactor: add collision helper method

--- alien_invasion.py
+++ alien_invasion.py
@@ -57,7 +57,9 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
 
+    def _check_bullet_alien_collisions(self):
         # KILL DETECTION
         collision = pygame.sprite.groupcollide(
             self.bullets, self.aliens, False, True)
